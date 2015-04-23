2f521a0876b4fbe74b78e04ae52f5654e86578b4
euna park <epark2746@columbia.edu>
2015-04-23 17:07:20 -0400

    edits for Thursday

--- js/main.js
+++ js/main.js
@@ -3,49 +3,60 @@
 var sns = [
 		{
 			name: 'Facebook',
-			visitors: '900 mil',
+			visitors: '900000000',
 			rank: 1,
 			image: 'facebook.png'
 		},
 		{
 			name: 'Twitter',
-			visitors: '310 mil',
+			visitors: '310000000',
 			rank: 2,
 			image: 'twitter.png'
 		},
 		{
 			name: 'Linkedin',
 			rank: 3,
-			visitors: '255 mil',
+			visitors: '255000000',
 			image: 'linkedin.png'
 		},
 		{
 			name: 'Pinterest',
 			rank: 4,
-			visitors: '250 mil',
+			visitors: '250000000',
 			image: 'pinterest.png'
 		},
 		{
 			name: 'Google Plus',
 			rank: 5,
-			visitors: '120 mil',
+			visitors: '120000000',
 			image: 'google.png'
 
 		},
 		{
 			name: 'Tumblr',
 			rank: 6,
-			visitors: '110 mil',
+			visitors: '110000000',
 			image: 'tumblr.png'
 		},
 		{
 			name: 'Instagram',
 			rank: 7,
-			visitors: '100 mil',
+			visitors: '100000000',
 			image: 'instagram.png'
 		}
 
 	];
+
+	var formatHelpers = {
+		addCommas: function(value){
+			return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		},
+
+		toUpperCase: function(name){
+			return name.toString().toUpperCase();
+		}
+
+	}
 
 
 	// Step 1: in the JavaScript, load the html from the template we made in index.html
@@ -57,12 +68,20 @@ var sns = [
 
 	// Step 3: Loop through our data and bake out all of our elements
 	sns.forEach(function(snsData){
+		_.extend(snsData, formatHelpers); 
+		//_.extend () merges two keys together 
+		// extends through the loop
+		
 		var all_sns_data = templateFactory(snsData);
-		console.log(all_sns_data)
+		console.log(snsData)
 		console.log('-----');
 		$('#container').append(all_sns_data);
 	});
 
 
 
+
 }).call(this);
+
+
+
